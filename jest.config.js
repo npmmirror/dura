04b1f9bfd77993ac42c9f7e02e45cf49c9d0198b
袁禹 <yuanyu@qianmi.com>
2019-02-03 21:53:23 +0800

    .

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
-  testPathIgnorePatterns: ["/node_modules/", "/examples/", "/lib/"],
+  testPathIgnorePatterns: ["/node_modules/", "/example/", "/lib/"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   collectCoverageFrom: ["packages/**/src/**/*.{ts,tsx,js,jsx}"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
