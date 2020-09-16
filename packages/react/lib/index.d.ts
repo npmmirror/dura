1531ad1c7c306c114f2fc44a49c8b3770f3f92f0
ityuany <yuanyu@shein.com>
2020-09-16 17:44:45 +0800

    修改main入口

--- packages/react/lib/index.d.ts
+++ packages/react/lib/index.d.ts
@@ -1,12 +1,44 @@
 import { defineStoreSlice } from "@dura/core";
-import type { ConfiguraOptions, JsonObject, ReducersMapOfStoreSlice, EffectsMapOfStoreSlice, StoreSlice, UnionToIntersection, ExtractAction, ExtractStateByStoreUnion, CreateStoreReturn } from "@dura/types";
+import type {
+  ConfiguraOptions,
+  JsonObject,
+  ReducersMapOfStoreSlice,
+  EffectsMapOfStoreSlice,
+  StoreSlice,
+  UnionToIntersection,
+  ExtractAction,
+  ExtractStateByStoreUnion,
+  CreateStoreReturn,
+} from "@dura/types";
 export interface Return<S, A> extends CreateStoreReturn<S, A> {
-    useMount: () => void;
-    useStore: <T>(deps?: T[]) => S;
-    useActions: () => A;
+  useMount: () => void;
+  useStore: <T>(deps?: T[]) => S;
+  useActions: () => A;
 }
 export interface Next<SS, A> {
-    <N extends string, S extends JsonObject, R extends ReducersMapOfStoreSlice<S>, E extends EffectsMapOfStoreSlice, STORES extends StoreSlice<N, S, R, E>[] = StoreSlice<N, S, R, E>[], GA = UnionToIntersection<ExtractAction<STORES[number]>>, GS = UnionToIntersection<ExtractStateByStoreUnion<STORES[number]>>>(...stores: STORES): Return<GS & SS, GA & A>;
+  <
+    N extends string,
+    S extends JsonObject,
+    R extends ReducersMapOfStoreSlice<S>,
+    E extends EffectsMapOfStoreSlice,
+    STORES extends StoreSlice<N, S, R, E>[] = StoreSlice<N, S, R, E>[],
+    GA = UnionToIntersection<ExtractAction<STORES[number]>>,
+    GS = UnionToIntersection<ExtractStateByStoreUnion<STORES[number]>>
+  >(
+    ...stores: STORES
+  ): Return<GS & SS, GA & A>;
 }
 export { defineStoreSlice };
-export declare function configura(options?: ConfiguraOptions): <N extends string, S extends JsonObject<any>, R extends ReducersMapOfStoreSlice<S>, E extends EffectsMapOfStoreSlice, STORES extends StoreSlice<N, S, R, E>[] = StoreSlice<N, S, R, E>[], GA = UnionToIntersection<ExtractAction<STORES[number]>>, GS = UnionToIntersection<ExtractStateByStoreUnion<STORES[number]>>>(...stores: STORES) => Next<GS, GA>;
+export declare function configura(
+  options?: ConfiguraOptions
+): <
+  N extends string,
+  S extends JsonObject<any>,
+  R extends ReducersMapOfStoreSlice<S>,
+  E extends EffectsMapOfStoreSlice,
+  STORES extends StoreSlice<N, S, R, E>[] = StoreSlice<N, S, R, E>[],
+  GA = UnionToIntersection<ExtractAction<STORES[number]>>,
+  GS = UnionToIntersection<ExtractStateByStoreUnion<STORES[number]>>
+>(
+  ...stores: STORES
+) => Next<GS, GA>;
