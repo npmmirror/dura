ccb7dc65717ddc024ef2b5e36c7907068e639901
ityuany <yuanyu@shein.com>
2020-09-19 21:39:25 +0800

    .

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
+const path = require("path");
 module.exports = {
   verbose: true,
   collectCoverage: true,
-  collectCoverageFrom: ["**/src/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
+  collectCoverageFrom: [
+    "packages/*/src/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+  ],
   coverageReporters: ["text", "lcov", "json"],
   transformIgnorePatterns: [],
 };
