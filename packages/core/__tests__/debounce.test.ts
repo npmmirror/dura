b551325cdbfc8f792cc90aae843476586b6ecb60
ityuany <yuanyu@shein.com>
2020-09-25 14:28:23 +0800

    test(节流、防抖、loading): 节流、防抖、loading测试用例补充

---
+++ packages/core/__tests__/debounce.test.ts
@@ -0,0 +1,60 @@
+import { defineStoreSlice, configura } from '../src';
+import { Action } from '@dura/types';
+
+const deloy = (ms: number) =>
+  new Promise((resolve, reject) => setTimeout(resolve, ms));
+const user = defineStoreSlice({
+  namespace: 'user',
+  state: {
+    name: 'default',
+  },
+  reducers: {
+    onChangeName(state, action: Action<{ name: string }>) {
+      state.name = action.payload.name;
+    },
+  },
+  effects: {
+    async asyncChangeName(action: Action<{ name: string }>) {
+      store.actions.user.onChangeName(action.payload);
+    },
+  },
+});
+const createStore = configura();
+const store = createStore(user);
+describe('test debounce', function () {
+  it('test plain debunce', function (done) {
+    store.actions.user.asyncChangeName({ name: 'xx1' }, { debounce: 50 });
+    store.actions.user.asyncChangeName({ name: 'xx2' }, { debounce: 50 });
+    store.actions.user.asyncChangeName({ name: 'xx3' }, { debounce: 50 });
+    expect(store.getState().user.name).toEqual('default');
+    setTimeout(() => {
+      expect(store.getState().user.name).not.toEqual('xx3');
+      expect(store.getState().user.name).toEqual('default');
+      setTimeout(() => {
+        expect(store.getState().user.name).toEqual('xx3');
+        done();
+      }, 10);
+    }, 40);
+  });
+
+  it('test iife debounce', function (done) {
+    const meta = {
+      debounce: {
+        wait: 50,
+        iife: true,
+      },
+    };
+    store.actions.user.asyncChangeName({ name: 'xx1' }, meta);
+    store.actions.user.asyncChangeName({ name: 'xx2' }, meta);
+    store.actions.user.asyncChangeName({ name: 'xx3' }, meta);
+    expect(store.getState().user.name).toEqual('xx1');
+    setTimeout(() => {
+      expect(store.getState().user.name).not.toEqual('xx3');
+      expect(store.getState().user.name).toEqual('xx1');
+      setTimeout(() => {
+        expect(store.getState().user.name).toEqual('xx3');
+        done();
+      }, 10);
+    }, 40);
+  });
+});
---
+++ packages/core/__tests__/loading.test.ts
@@ -0,0 +1,74 @@
+import { defineStoreSlice, configura } from '../src';
+let name = 'default';
+const user = defineStoreSlice({
+  namespace: 'user',
+  state: {
+    /** 姓名 */
+    name: '张三',
+  },
+  reducers: {
+    onChangeName(state, action) {
+      state.name = action.payload.name;
+    },
+  },
+  effects: {
+    async onAsyncQuery(action) {
+      name = action.payload.name;
+      await deloy(500);
+    },
+  },
+
+  watchs: {
+    async hello0(state, prevState) {
+      console.log(state.name);
+    },
+
+    hello: {
+      handler: async function (state, prevState) {
+        console.log(state.name);
+      },
+      immediate: true,
+    },
+  },
+});
+
+const deloy = (ms: number) =>
+  new Promise((resolve, reject) => setTimeout(resolve, ms));
+
+describe('test core loading', function () {
+  it('test customize loading', function (done) {
+    const createStore = configura();
+    const store = createStore(user);
+
+    const id = 11;
+
+    store.actions.user.onAsyncQuery(null, {
+      loading: {
+        customizeId: id,
+      },
+    });
+    expect(
+      store.getState().DURA.LOADING.user.onAsyncQuery.customize[id].status,
+    ).toBeTruthy();
+
+    setTimeout(() => {
+      expect(
+        store.getState().DURA.LOADING.user.onAsyncQuery.customize[id].status,
+      ).toBeFalsy();
+      done();
+    }, 501);
+  });
+
+  it('test loading', function (done) {
+    const createStore = configura();
+    const store = createStore(user);
+    store.actions.user.onAsyncQuery(null, { loading: true });
+    expect(store.getState().DURA.LOADING.user.onAsyncQuery.status).toBeTruthy();
+    setTimeout(() => {
+      expect(
+        store.getState().DURA.LOADING.user.onAsyncQuery.status,
+      ).toBeFalsy();
+      done();
+    }, 501);
+  });
+});
