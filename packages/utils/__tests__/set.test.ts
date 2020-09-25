7d8b3248fe7ce373bdd9b253e77b378e2dd364b5
ityuany <yuanyu@shein.com>
2020-09-25 13:55:06 +0800

    docs(docs): docs

--- packages/utils/__tests__/set.test.ts
+++ packages/utils/__tests__/set.test.ts
@@ -2,7 +2,9 @@ import { set } from '../src';
 
 describe('test set', function () {
   it('test set', function () {
-    let state = {};
+    let state = {
+      a: {},
+    };
     expect(state['a']?.['b']?.['c']?.['d']).toBeUndefined();
     set(state, ['a', 'b', 'c', 'd'], 12);
     expect(state['a']['b']['c']['d']).toEqual(12);
