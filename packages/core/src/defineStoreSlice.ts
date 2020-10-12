3c8cc9e7bf7bad255d3f62dbcc925f1ee4f8eea0
ityuany <yuanyu@shein.com>
2020-10-12 13:26:19 +0800

    全新logo

--- packages/core/src/defineStoreSlice.ts
+++ packages/core/src/defineStoreSlice.ts
@@ -26,13 +26,6 @@ export type WrapStoreSlice<N, S, R, E> = {
   effects: E;
 };
 
-// export function defineStoreSlice<
-//   N extends string,
-//   S extends JsonObject,
-//   R extends ReducersMapOfStoreSlice<S>,
-//   E extends EffectsMapOfStoreSlice
-// >(store: StoreSlice<N, S, R, E>): StoreSlice<N, S, R, E>;
-
 export function defineStoreSlice<
   N extends string,
   S extends JsonObject,
