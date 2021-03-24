7bc1b872a4ee2264b0d3abd03b0b6d496bd1f575
ityuany <519495771@qq.com>
2021-03-25 01:05:51 +0800

    .

--- src/run.ts
+++ src/run.ts
@@ -235,3 +235,58 @@ const _ = createDura();
 const res = createStore((state = {}) => state, compose(_));
 
 res.createSlice;
+function name<
+  S,
+  M extends {
+    namespace: string;
+    state: S;
+    reducers: { [name: string]: (state: S) => any };
+  }
+>(
+  params: M,
+): {
+  [K in keyof M['reducers'] &
+    string as `use${Capitalize<K>}`]: M['reducers'][K];
+} {
+  return null as never;
+}
+
+const r = name(<const>{
+  namespace: 'sss',
+  state: { name: '' },
+  reducers: { changeName: (state) => state },
+});
+
+type PathKeys<T> = object extends T
+  ? string
+  : T extends readonly any[]
+  ? Extract<keyof T, `${number}`> | SubKeys<T, Extract<keyof T, `${number}`>>
+  : T extends object
+  ? Extract<keyof T, string> | SubKeys<T, Extract<keyof T, string>>
+  : never;
+type SubKeys<T, K extends string> = K extends keyof T
+  ? `${K}.${PathKeys<T[K]>}`
+  : never;
+type PropType<T, Path extends string> = Path extends keyof T
+  ? T[Path]
+  : Path extends `${infer K}.${infer R}`
+  ? K extends keyof T
+    ? PropType<T[K], R>
+    : unknown
+  : unknown;
+
+declare function getProp<T, P extends PathKeys<T>>(
+  obj: T,
+  path: P,
+): PropType<T, P>;
+
+const obj = {
+  name: 'John',
+  age: 42,
+  cars: [
+    { make: 'Ford', age: 10 },
+    { make: 'Trabant', age: 35 },
+  ],
+} as const;
+
+let make = getProp(obj, 'cars.1.age');
