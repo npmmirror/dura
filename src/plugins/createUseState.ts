cea350cc65e1f6e3d46a1384f34e1b48d4f38c8b
ityuany <519495771@qq.com>
2021-04-14 22:42:17 +0800

    review useState

--- src/plugins/createUseState.ts
+++ src/plugins/createUseState.ts
@@ -4,85 +4,115 @@ import { useUpdate, useMemoized, usePersistFn } from '@onecocjs/use';
 import { Context, Selector } from '../types';
 import { createProxy } from '../createProxy';
 
-export function createUseState({ namespace, reduxStore }: Context) {
-  return function useState(selector: Selector | boolean = true) {
-    // const selector = options?.selector;
-    const update = useUpdate();
-    // immer proxy of state
-    const refStateProxy = useRef(undefined);
-    // state
-    const refState = useRef(undefined);
-    // deps
-    const refDeps = useMemoized(() => new Map<string, unknown>());
-    // selector state
-    const refSelectorState = useRef(undefined);
+/**
+ * 依赖收集的逻辑
+ */
+function useAutoSubscribe({ namespace, reduxStore }: Context) {
+  const update = useUpdate();
+  // immer proxy of state
+  const refStateProxy = useRef(undefined);
+  // state
+  const refState = useRef(undefined);
+  // deps
+  const refDeps = useMemoized(() => new Map<string, unknown>());
 
-    // cache selector state
+  function recording() {
+    //获取当前节点的 redux 数据
+    const state = reduxStore.getState()?.[namespace];
+    //清空过时的依赖缓存
+    refDeps.clear();
+    //每次都缓存一下最新的redux数据
+    refState.current = state;
+    //创建 proxy 代理对象
+    refStateProxy.current = createProxy(state, refDeps);
+  }
+
+  const subscribe = () => {
+    const currentState = reduxStore.getState()[namespace];
+    const keysIterator = refDeps.keys();
+    let current = keysIterator.next();
+    let isUpdate = false;
+    while (!current.done) {
+      const thePath = current.value;
+
+      if (get(currentState, thePath) !== get(refState.current, thePath)) {
+        isUpdate = true;
+        break;
+      }
+      current = keysIterator.next();
+    }
+    isUpdate && update();
+  };
+
+  return { recording, subscribe, state: () => refStateProxy.current };
+}
+
+/**
+ * 包含两种模式
+ * 如果 selector 是一个函数， 那么则采用 shallowEqual diff模式，
+ * 否则 采用癫狂模式渲染， 理论上 如果 selector 不是一个函数，那么值一定为 true，
+ * 这应当由外部逻辑控制
+ */
+function useSelectorSubscribe(
+  { namespace, reduxStore }: Context,
+  selector: Selector | boolean,
+) {
+  const update = useUpdate();
+  // selector state
+  const refSelectorState = useRef(undefined);
+  function recording() {
     if (typeof selector === 'function') {
-      refSelectorState.current = selector?.(reduxStore.getState()[namespace]);
+      refSelectorState.current = selector(reduxStore.getState()[namespace]);
+    } else {
+      refSelectorState.current = reduxStore.getState()[namespace];
     }
-
-    function recording() {
-      //获取当前节点的 redux 数据
-      const state = reduxStore.getState()?.[namespace];
-      //清空过时的依赖缓存
-      refDeps.clear();
-      //每次都缓存一下最新的redux数据
-      refState.current = state;
-      //创建 proxy 代理对象
-      refStateProxy.current = createProxy(state, refDeps);
-    }
-
-    // TODO 这里注意一下，有可能需要 优化至 仅仅首次才 recording 一次
-    recording();
-
-    const depAutoSubscribe = usePersistFn(() => {
+  }
+  const subscribe = () => {
+    if (typeof selector === 'function') {
       const currentState = reduxStore.getState()[namespace];
-      const keysIterator = refDeps.keys();
-      let current = keysIterator.next();
+      const selectorState = selector!(currentState);
+      const keys = Object.keys(selectorState);
       let isUpdate = false;
-      while (!current.done) {
-        const thePath = current.value;
-
-        if (get(currentState, thePath) !== get(refState.current, thePath)) {
+      for (let index = 0; index < keys.length; index++) {
+        const theKey = keys[index];
+        if (get(selectorState, theKey) !== get(refSelectorState, theKey)) {
           isUpdate = true;
           break;
         }
-        current = keysIterator.next();
       }
       isUpdate && update();
-    });
+    }
+  };
+  return { recording, subscribe, state: () => refSelectorState.current };
+}
 
-    const selectorSubscribe = usePersistFn(() => {
-      if (typeof selector === 'function') {
-        const currentState = reduxStore.getState()[namespace];
-        const selectorState = selector!(currentState);
-        const keys = Object.keys(selectorState);
-        let isUpdate = false;
-        for (let index = 0; index < keys.length; index++) {
-          const theKey = keys[index];
-          if (get(selectorState, theKey) !== get(refSelectorState, theKey)) {
-            isUpdate = true;
-            break;
-          }
-        }
-        isUpdate && update();
-      }
-    });
+export function createUseState(context: Context) {
+  return function useState(selector: Selector | boolean = true) {
+    /**
+     * 依赖收集的 Subscribe
+     */
+    const autoSubscribeContext = useAutoSubscribe(context);
+    /**
+     * selector 与 癫狂渲染的组合体
+     * 当 selector 是一个 函数时， 那么则根据 selector 的返回值进行 shallowEqual diff，
+     * 否则 状态的每一次变更都会导致渲染。
+     */
+    const selectorSubscribeContext = useSelectorSubscribe(context, selector);
 
-    const subscribeState = usePersistFn(() => {
-      if (selector === true) {
-        depAutoSubscribe();
-      } else if (typeof selector === 'function') {
-        selectorSubscribe();
-      } else {
-        // if selector is false and other
-        update();
-      }
-    });
+    /**
+     * 如果 selector 为 false ， 那么 有可能是 基于 selector 的 shallowEqual diff ，
+     * 否则 是 癫狂渲染模式 ，这里需要保证，如果 selector 为 true ， 那么一定不可以走
+     * useSelectorSubscribe
+     */
+    let { subscribe, state, recording } =
+      selector === true ? autoSubscribeContext : selectorSubscribeContext;
+    /** 记录 */
+    recording();
+    /** 持久化的订阅函数，为了避免反复装卸 redux subscribe */
+    const subscribeFn = usePersistFn(subscribe);
 
-    useEffect(() => reduxStore.subscribe(subscribeState), [subscribeState]);
+    useEffect(() => context.reduxStore.subscribe(subscribeFn), [subscribeFn]);
 
-    return refStateProxy.current;
+    return state();
   };
 }
